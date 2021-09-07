9b194ab156a851b3f41ee68405d6b9dc826d30df
savilard <vitaliyantonoff@gmail.com>
2021-09-07 10:04:33 +0600

    feat: combine order creation into transaction

--- foodcartapp/views.py
+++ foodcartapp/views.py
@@ -1,3 +1,4 @@
+from django.db import transaction
 from django.http import JsonResponse
 from django.templatetags.static import static
 from rest_framework import status
@@ -59,6 +60,7 @@ def product_list_api(request):
     return Response(dumped_products)
 
 
+@transaction.atomic
 @api_view(['POST'])
 def register_order(request):
     serializer = OrderSerializer(data=request.data)
