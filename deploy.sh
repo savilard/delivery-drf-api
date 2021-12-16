e9d7c17120f37b591bb1e305a8516115f5f92a75
Vitaliy Antonov <60862933+savilard@users.noreply.github.com>
2021-12-16 10:40:39 +0600

    deploy(docker): optimize deploy (#64)

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,15 @@
 REVISION=$(git rev-parse --verify HEAD)
 
 git pull
-DOCKER_BUILDKIT=1 docker-compose -f docker-compose.prod.yml up --build --force-recreate --no-deps -d parcel
-DOCKER_BUILDKIT=1 docker-compose -f docker-compose.prod.yml up --build --force-recreate --no-deps -d server
+
+DOCKER_BUILDKIT=1 docker compose -f docker-compose.prod.yml up --build --force-recreate --no-deps -d server
+docker compose cp server:/opt/app/staticfiles/. ./frontend
+
+DOCKER_BUILDKIT=1 docker compose -f docker-compose.prod.yml up --build --force-recreate --no-deps -d parcel
+docker compose cp parcel:/opt/app/bundles/. ./frontend
+
+docker compose cp ./frontend/. nginx:/opt/app/frontend
+
 docker compose exec server python manage.py migrate --noinput
 docker compose exec server http POST https://api.rollbar.com/api/1/deploy/ access_token=$ROLLBAR_ACCESS_TOKEN environment=$ROLLBAR_ENVIRONMENT revision=$REVISION
 echo "Deploy successfully completed"
