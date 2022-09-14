f17540ccfdef54415587d29a77fb9063fa512c2b
savilard <vitaliyantonoff@gmail.com>
2022-09-14 12:25:15 +0600

    :recycle: Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,7 +3,9 @@ set -e
 
 docker compose -f docker-compose.yml up frontend --build --force-recreate --no-deps
 docker compose cp frontend:/opt/app/bundles/. ./src/backend/bundles
-docker compose -f docker-compose.yml up backend db --build --force-recreate -d --no-deps
+docker compose -f docker-compose.yml up backend --build --force-recreate -d --no-deps
 
-docker compose run backend python manage.py migrate --noinput
-docker compose run backend python manage.py createsuperuser --noinput
+docker compose up db -d
+docker compose run --rm backend sh -c "python manage.py wait_for_db"
+docker compose exec db psql -U app_dev -f /backup/postgres-test-data.sql
+docker compose down
